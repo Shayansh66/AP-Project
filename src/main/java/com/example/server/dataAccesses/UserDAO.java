8659a219bad0b362a51688c5c5667e0818653ef0
Shayansh66 <shahmohammadish66@gmail.com>
2024-05-27 23:14:54 +0330

    feat : UserDAO class basics added. refactor : in User class A&M method for city and country added

--- src/main/java/com/example/server/dataAccesses/UserDAO.java
+++ src/main/java/com/example/server/dataAccesses/UserDAO.java
@@ -1,5 +1,7 @@
 package main.java.com.example.server.dataAccesses;
 
+import main.java.com.example.server.models.User;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
@@ -20,5 +22,44 @@ public class UserDAO {
         statement = theConnection.prepareStatement("ALTER TABLE users AUTO_INCREMENT = 20000000");
         statement.executeUpdate();
     }
+
+    public void saveUser(User user) throws SQLException {
+        PreparedStatement statement = theConnection.prepareStatement("INSERT INTO users (firstname, lastname, additionalname, headtitle, country, city) VALUES (?, ?, ?, ?, ?, ?)");
+        statement.setString(1, user.getFirstName());
+        statement.setString(2, user.getLastname());
+        statement.setString(3, user.getAdditionalname());
+        statement.setString(4, user.getHeadtitle());
+        statement.setString(5, user.getCountry());
+        statement.setString(6, user.getCity());
+        statement.executeUpdate();
+    }
+
+    public void updateUser(User user) throws SQLException {
+        PreparedStatement statement = theConnection.prepareStatement("UPDATE users SET firstname = ?, lastname = ?, additionalname = ?, headtitle = ?, country = ?, city = ? WHERE userid = ?");
+        statement.setString(1, user.getFirstName());
+        statement.setString(2, user.getLastname());
+        statement.setString(3, user.getAdditionalname());
+        statement.setString(4, user.getAdditionalname());
+        statement.setString(5, user.getCountry());
+        statement.setString(6, user.getCity());
+        statement.setInt(7, user.getId());
+        statement.executeUpdate();
+    }
+
+    public void deleteUser(User user) throws SQLException {
+        PreparedStatement statement = theConnection.prepareStatement("DELETE FROM users WHERE userid = ?");
+        statement.setInt(1, user.getId());
+        statement.executeUpdate();
+    }
     
+    public void deleteUser(int id) throws SQLException {
+        PreparedStatement statement = theConnection.prepareStatement("DELETE FROM users WHERE userid = ?");
+        statement.setInt(1, id);
+        statement.executeUpdate();
+    }
+    
+    public void deleteUsers() throws SQLException {
+        PreparedStatement statement = theConnection.prepareStatement("DELETE FROM users");
+        statement.executeUpdate();
+    }
 }
--- src/main/java/com/example/server/models/User.java
+++ src/main/java/com/example/server/models/User.java
@@ -80,6 +80,22 @@ public class User {
     public void setHeadtitle(String headtitle) {
         this.headtitle = headtitle;
     }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
     
     
     @Override
