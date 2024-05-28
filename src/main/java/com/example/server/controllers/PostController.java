441627678936a7ed807cd3aa04f737f591ee13b8
SinaRiot <sina.khan9714@gmail.com>
2024-05-28 22:39:00 +0330

    added postcontroller

---
+++ src/main/java/com/example/server/controllers/PostController.java
@@ -0,0 +1,16 @@
+package main.java.com.example.server.controllers;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import main.java.com.example.server.dataAccesses.PostDAO;
+import main.java.com.example.server.models.Post;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class PostController {
+    
+}
