dc4949219abf704db3db016c351b81c56987846f
Shayansh66 <shahmohammadish66@gmail.com>
2024-06-01 21:16:38 +0330

    ContactDAO file completed

--- src/main/java/com/example/server/dataAccesses/ContactDAO.java
+++ src/main/java/com/example/server/dataAccesses/ContactDAO.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 
 public class ContactDAO {
@@ -31,9 +32,9 @@ public class ContactDAO {
         statement.setString(3, contact.getEmail());
         statement.setString(4, contact.getPhonenumber());
         statement.setString(5, contact.getPhoneType());
-        statement.setString(6, contact.getAddres());
+        statement.setString(6, contact.getAddress());
         statement.setTimestamp(7, contact.getBirthday());
-        statement.setString(8, contact.getbirthDayVisibility());
+        statement.setString(8, contact.getBirthDayVisibility());
         statement.setString(9, contact.getCommunicationId());
         statement.executeUpdate();
     }
@@ -45,9 +46,9 @@ public class ContactDAO {
         statement.setString(3, contact.getEmail());
         statement.setString(4, contact.getPhonenumber());
         statement.setString(5, contact.getPhoneType());
-        statement.setString(6, contact.getAddres());
+        statement.setString(6, contact.getAddress());
         statement.setTimestamp(7, contact.getBirthday());
-        statement.setString(8, contact.getbirthDayVisibility());
+        statement.setString(8, contact.getBirthDayVisibility());
         statement.setString(9, contact.getCommunicationId());
         statement.setInt(9, contact.getId());
         statement.executeUpdate();
@@ -68,6 +69,78 @@ public class ContactDAO {
     public void deleteContacts() throws SQLException {
         PreparedStatement statement = thConnection.prepareStatement("DELETE FROM contacts;");
         statement.executeUpdate();
+    }
+    
+    public Contact getContactById(String id) throws SQLException {
+        PreparedStatement statement = thConnection.prepareStatement("SELECT * FROM contacts WHERE id = ?;");
+        statement.setInt(1, Integer.parseInt(id));
+        ResultSet resultSet = statement.executeQuery();
+
+        if (resultSet.next()) {
+            Contact contact = new Contact();
+            contact.setId(resultSet.getInt("id"));
+            contact.setUserid(resultSet.getInt("userid"));
+            contact.setProfilelink(resultSet.getString("profilelink"));
+            contact.setEmail(resultSet.getString("email"));
+            contact.setPhonenumber(resultSet.getString("phonenumber"));
+            contact.setPhoneType(resultSet.getString("phonetype"));
+            contact.setAddress(resultSet.getString("address"));
+            contact.setBirthday(resultSet.getTimestamp("birthday"));
+            contact.setBirthday(resultSet.getTimestamp("birthday"));
+            contact.setBirthDayVisibility(resultSet.getString("birthdayvisibility"));
+            contact.setCommunicationId(resultSet.getString("communicationidsetCommunicationId(id);"));
+            return contact;
+        }
+
+        return null;
+    }
+
+    public Contact getContactByUserId(String userid) throws SQLException {
+        PreparedStatement statement = thConnection.prepareStatement("SELECT * FROM contacts WHERE userid = ?;");
+        statement.setInt(1, Integer.parseInt(userid));
+        ResultSet resultSet = statement.executeQuery();
+
+        if (resultSet.next()) {
+            Contact contact = new Contact();
+            contact.setId(resultSet.getInt("id"));
+            contact.setUserid(resultSet.getInt("userid"));
+            contact.setProfilelink(resultSet.getString("profilelink"));
+            contact.setEmail(resultSet.getString("email"));
+            contact.setPhonenumber(resultSet.getString("phonenumber"));
+            contact.setPhoneType(resultSet.getString("phonetype"));
+            contact.setAddress(resultSet.getString("address"));
+            contact.setBirthday(resultSet.getTimestamp("birthday"));
+            contact.setBirthday(resultSet.getTimestamp("birthday"));
+            contact.setBirthDayVisibility(resultSet.getString("birthdayvisibility"));
+            contact.setCommunicationId(resultSet.getString("communicationidsetCommunicationId(id);"));
+            return contact;
+        }
+
+        return null;
+    }
+
+    public ArrayList<Contact> getContacts() throws SQLException {
+        ArrayList<Contact> list = new ArrayList< >();
+        PreparedStatement statement = thConnection.prepareStatement("SELECT * FROM contacts;");
+        ResultSet resultSet = statement.executeQuery();
+
+        while (resultSet.next()) {
+            Contact contact = new Contact();
+            contact.setId(resultSet.getInt("id"));
+            contact.setUserid(resultSet.getInt("userid"));
+            contact.setProfilelink(resultSet.getString("profilelink"));
+            contact.setEmail(resultSet.getString("email"));
+            contact.setPhonenumber(resultSet.getString("phonenumber"));
+            contact.setPhoneType(resultSet.getString("phonetype"));
+            contact.setAddress(resultSet.getString("address"));
+            contact.setBirthday(resultSet.getTimestamp("birthday"));
+            contact.setBirthday(resultSet.getTimestamp("birthday"));
+            contact.setBirthDayVisibility(resultSet.getString("birthdayvisibility"));
+            contact.setCommunicationId(resultSet.getString("communicationidsetCommunicationId(id);"));
+            list.add(contact);
+        }
+
+        return list;
     }
     
 }
