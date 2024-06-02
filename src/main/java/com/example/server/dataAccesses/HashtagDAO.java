e9d97154e975497c3b8698a8c9cfb3c40fe89c81
Shayansh66 <shahmohammadish66@gmail.com>
2024-06-02 23:33:53 +0330

    Hashtag deleted. HahstagDAO fully handled and completed

--- src/main/java/com/example/server/dataAccesses/HashtagDAO.java
+++ src/main/java/com/example/server/dataAccesses/HashtagDAO.java
@@ -1,7 +1,5 @@
 package main.java.com.example.server.dataAccesses;
 
-import main.java.com.example.server.models.Hashtag;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -19,10 +17,56 @@ public class HashtagDAO {
     }
     
     public void createTable() throws SQLException {
-        PreparedStatement statement = theConnection.prepareStatement("CRAATE TABLE IF NOT EXISTS hashtags (id INT PRIMARY KEY AUTO_INCREMENT, content VARCHAR(255));");
+        PreparedStatement statement = theConnection.prepareStatement("CRAATE TABLE IF NOT EXISTS hashtags (postid INT NOT NULL, content VARCHAR(255) PRIMARY KEY);");
         statement.executeUpdate();
-        statement = theConnection.prepareStatement("ALTER TABLE hashtags AUTO_INCREMENT = 26000000;");
+    }
+
+    public void saveHashtag(String postid, String content) throws SQLException {
+        PreparedStatement statement = theConnection.prepareStatement("INSERT INTO hashtags (postid, content) VALUES (?, ?);");
+        statement.setInt(1, Integer.parseInt(postid));
+        statement.setString(2, content);
         statement.executeUpdate();
+    }
+
+    public void deleteHashtag(String content) throws SQLException {
+        PreparedStatement statement = theConnection.prepareStatement("DELETE FROM hashtags WHERE content = ?;");
+        statement.setString(1, content);
+        statement.executeUpdate();
+    }
+
+    public void deleteHashtags(String postid) throws SQLException {
+        PreparedStatement statement = theConnection.prepareStatement("DELETE FROM hashtags WHERE postid = ?;");
+        statement.setInt(1, Integer.parseInt(postid));
+        statement.executeUpdate();
+    }
+
+    public void deleteHashtags() throws SQLException {
+        PreparedStatement statement = theConnection.prepareStatement("DELETE FROM hashtags;");
+        statement.executeUpdate();
+    }
+
+    public ArrayList<Integer> getHashtagsPost(String postid) throws SQLException {
+        ArrayList<Integer> list = new ArrayList< >();
+        PreparedStatement statement = theConnection.prepareStatement("SELECT * FROM hashtags WHERE postid = ?;");
+        statement.setInt(1, Integer.parseInt(postid));
+        ResultSet resultSet = statement.executeQuery();
+
+        while (resultSet.next()) {
+            list.add(resultSet.getInt("postid"));
+        }
+        return list;
+    }
+
+    public ArrayList<Integer> getPosts(String content) throws SQLException {
+        ArrayList<Integer> list = new ArrayList< >();
+        PreparedStatement statement = theConnection.prepareStatement("SELECT * FROM hashtags WHERE content = ?;");
+        statement.setString(1, content);
+        ResultSet resultSet = statement.executeQuery();
+
+        while (resultSet.next()) {
+            list.add(resultSet.getInt("postid"));
+        }
+        return list;
     }
     
 }
