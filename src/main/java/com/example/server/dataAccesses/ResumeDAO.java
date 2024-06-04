9a53c3b0971ddd4f614d0a94ee5aae43bb587fa4
Shayansh66 <shahmohammadish66@gmail.com>
2024-06-04 21:16:38 +0330

    ResumeDAO file completed

--- src/main/java/com/example/server/dataAccesses/ResumeDAO.java
+++ src/main/java/com/example/server/dataAccesses/ResumeDAO.java
@@ -25,6 +25,94 @@ public class ResumeDAO {
         statement.executeUpdate();
     }
 
+    public void saveResume(Resume resume) throws SQLException {
+        PreparedStatement statement = theConnection.prepareStatement("INSERT INTO resumes (ownerid, positionid, resumefile, description) VALUES (?, ?, ?, ?);");
+        statement.setInt(1, resume.getOwnerId());
+        statement.setInt(2, resume.getPositionId());
+        statement.setString(3, resume.getResumeFile());
+        statement.setString(4, resume.getDescription());
+        statement.executeUpdate();
+    }
     
+    public void updateResume(Resume resume) throws SQLException {
+        PreparedStatement statement = theConnection.prepareStatement("UPDATE resumes SET ownerid = ?, positionid = ?, resumefile = ?, description = ?;");
+        statement.setInt(1, resume.getOwnerId());
+        statement.setInt(2, resume.getPositionId());
+        statement.setString(3, resume.getResumeFile());
+        statement.setString(4, resume.getDescription());
+        statement.executeUpdate();
+    }
+
+    public void deleteResume(Resume resume) throws SQLException {
+        PreparedStatement statement = theConnection.prepareStatement("DELETE FROM resumes WHERE id = ?;");
+        statement.setInt(1, resume.getId());
+        statement.executeUpdate();
+    }
+
+    public void deleteResume(int id) throws SQLException {
+        PreparedStatement statement = theConnection.prepareStatement("DELETE FROM resumes WHERE id = ?;");
+        statement.setInt(1, id);
+        statement.executeUpdate();
+    }
+
+    public void deleteResumes() throws SQLException {
+        PreparedStatement statement = theConnection.prepareStatement("DELETE FROM resumes;");
+        statement.executeUpdate();
+    }
+
+    public Resume getResume(int id) throws SQLException {
+        PreparedStatement statement = theConnection.prepareStatement("SELECT * FROM resumes WHERE id = ?;");
+        statement.setInt(1, id);
+        ResultSet resultSet = statement.executeQuery();
+
+        if (resultSet.next()) {
+            Resume resume = new Resume();
+            resume.setId(resultSet.getInt("id"));
+            resume.setOwnerId(resultSet.getInt("ownerid"));
+            resume.setPositionId(resultSet.getInt("positionid"));
+            resume.setResumeFile(resultSet.getString("resumefile"));
+            resume.setDescription(resultSet.getString("description"));
+            return resume;
+        }
+
+        return null;
+    }
+    
+    public ArrayList<Resume> getResumesByUserId(int userid) throws SQLException {
+        ArrayList<Resume> list = new ArrayList< >();
+        PreparedStatement statement = theConnection.prepareStatement("SELECT * FROM resumes WHERE userid = ?;");
+        statement.setInt(1, userid);
+        ResultSet resultSet = statement.executeQuery();
+
+        while (resultSet.next()) {
+            Resume resume = new Resume();
+            resume.setId(resultSet.getInt("id"));
+            resume.setOwnerId(resultSet.getInt("ownerid"));
+            resume.setPositionId(resultSet.getInt("positionid"));
+            resume.setResumeFile(resultSet.getString("resumefile"));
+            resume.setDescription(resultSet.getString("description"));
+            list.add(resume);
+        }
+
+        return list;
+    }
+    
+    public ArrayList<Resume> getResumes() throws SQLException {
+        ArrayList<Resume> list = new ArrayList< >();
+        PreparedStatement statement = theConnection.prepareStatement("SELECT * FROM resumes;");
+        ResultSet resultSet = statement.executeQuery();
+
+        while (resultSet.next()) {
+            Resume resume = new Resume();
+            resume.setId(resultSet.getInt("id"));
+            resume.setOwnerId(resultSet.getInt("ownerid"));
+            resume.setPositionId(resultSet.getInt("positionid"));
+            resume.setResumeFile(resultSet.getString("resumefile"));
+            resume.setDescription(resultSet.getString("description"));
+            list.add(resume);
+        }
+
+        return list;
+    }
 
 }
