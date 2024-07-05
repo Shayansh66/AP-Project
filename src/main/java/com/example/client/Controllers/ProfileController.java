b00c74e3e9206ea804030f5089bbb8bda331af66
Shayansh66 <shahmohammadish66@gmail.com>
2024-07-05 13:11:40 +0330

    Conacts added

--- src/main/java/com/example/client/Controllers/ProfileController.java
+++ src/main/java/com/example/client/Controllers/ProfileController.java
@@ -1,8 +1,34 @@
 package main.java.com.example.client.Controllers;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.stage.Stage;
 
 public class ProfileController {
     
-    
+    @FXML
+    private Hyperlink contactsHyperlink;
+
+
+    public void showContacts(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/main/Resource/com/example/client/Contacts.fxml"));
+            Scene scene = new Scene(root);
+
+                                                                /*************************/
+
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     
 }
